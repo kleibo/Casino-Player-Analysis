86d2afcd605a340b2c070a322c54d55535bbc480
kleibo <kleibo1999@gmail.com>
2023-01-22 18:04:32 -0500

    Linear Regression methods refactored into their own class

--- PlayerAnalysis.py
+++ PlayerAnalysis.py
@@ -23,55 +23,9 @@ class PlayerAnalysis:
         descriptiveStats = data.describe()
         return descriptiveStats
 
-    def linearRegressionByPlayer(self):
-        self.playerData['GamingDt'] = pd.to_datetime(self.playerData['GamingDt'])
-        self.playerData.sort_values(by='GamingDt', inplace=True)
-        self.playerData['GamingDt_order'] = self.playerData.groupby('PlayerId')['GamingDt'].rank()
-
-        # Create a dictionary to store the models
-        models = {}
-        
-        # Group the data by player and fit a linear regression model
-        for player, group in self.playerData.groupby('PlayerId'):
-            x = group['GamingDt_order'].values.reshape(-1, 1)
-            y = group['TotlTheo'].values
-            model = LinearRegression().fit(x, y)
-            models[player] = model
-        return models
-
-
-    def plotLinearRegression(self, models):
-        # Create a drop-down list to select the player
-        playerList = list(models.keys())
-        root = tk.Tk()
-        root.title("Linear Regression")
-        playerDropDown = ttk.Combobox(root, values=playerList)
-        playerDropDown.current(0)
-        playerDropDown.pack()
-        
-        # Plot the linear regression for the selected player
-        def onPlayerChange(*args):
-            plt.figure()
-            player = int(playerDropDown.get())
-            model = models[player]
-            data = self.playerData[self.playerData['PlayerId'] == player]
-            x = data['GamingDt_order'].values.reshape(-1, 1)
-            y = data['TotlTheo'].values
-            plt.scatter(x,y)
-            plt.plot(x, model.predict(x), color = 'red')
-            plt.xlabel('Gaming Date Order')
-            plt.ylabel('Total Theo')
-            plt.title(f'Linear Regression for player {player}')
-            plt.show()
-
-        playerDropDown.bind("<<ComboboxSelected>>", onPlayerChange)
-        root.mainloop()
-        
         
 
 
-analysis = PlayerAnalysis("/Users/kleib/Code/kleibo/Projects/Casino-Player-Analysis/Test_Data_10_Accts.csv")
-models = analysis.linearRegressionByPlayer()
-analysis.plotLinearRegression(models)
+
 
 
