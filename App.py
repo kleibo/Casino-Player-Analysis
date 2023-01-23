89fc9c9ff2bd1a5bd05bac7f1465236e90eca390
kleibo <kleibo1999@gmail.com>
2023-01-22 20:30:49 -0500

    Ran linear regression function through class at app.py

--- App.py
+++ App.py
@@ -1,17 +1,19 @@
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import PlayerAnalysis
-
+import LinearRegression
 # Open a file dialog to select the player data file
 root = Tk()
 root.withdraw()
 filepath = askopenfilename()
+root.protocol("WM_DELETE_WINDOW", root.destroy)
+print(filepath)
+
 
 # Create a PlayerAnalysis object with filepath directory
-analysis = PlayerAnalysis.PlayerAnalysis(filepath)
-
-analysis.linearRegressionByPlayer()
-
+analysis = LinearRegression.PlayerLinearRegression(filepath)
+models = analysis.linearRegressionByPlayer()
+analysis.plotLinearRegression(models)
 
 # Save Results
 # clustering.saveResults("Model")
