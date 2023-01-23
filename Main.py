40ed82e98c1fd71da1edf7710e21917f1d7988a0
kleibo <kleibo1999@gmail.com>
2023-01-22 23:26:15 -0500

    Finalized first version

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 from PlayerAnalysis import PlayerAnalysis
 from LinearRegression import PlayerLinearRegression
@@ -6,11 +7,35 @@ from KmeansClustering import PlayerKmeansClustering
 
 class Main:
     def __init__(self):
+        self.filepath = ""
+        
+        # Create the main window
+        self.root = tk.Tk()
+        self.root.title("Player Analysis")
+        
+        # Create a button for each method
+        self.selectFileButton = tk.Button(self.root, text="Select File", command=self.selectFile)
+        self.selectFileButton.pack()
+        
+        self.runPlayerAnalysisButton = tk.Button(self.root, text="Run Player Analysis", command=self.runPlayerAnalysis)
+        self.runPlayerAnalysisButton.pack()
+        
+        self.runLinearRegressionButton = tk.Button(self.root, text="Run Linear Regression", command=self.runLinearRegression)
+        self.runLinearRegressionButton.pack()
+        
+        self.runKmeansClusteringButton = tk.Button(self.root, text="Run Kmeans Clustering", command=self.runKmeansClustering)
+        self.runKmeansClusteringButton.pack()
+        
+        # Start the main loop
+        self.root.mainloop()
+    
+    def selectFile(self):
         # Open a file dialog to select the player data file
-        root = tk.Tk()
-        root.withdraw()
+        self.root = tk.Tk()
+        self.root.withdraw()
         self.filepath = askopenfilename()
-        root.protocol("WM_DELETE_WINDOW", root.destroy)
+        self.root.protocol("WM_DELETE_WINDOW", self.root.destroy)
+
 
     def runPlayerAnalysis(self):
         file = PlayerAnalysis(self.filepath)
@@ -29,7 +54,7 @@ class Main:
 # Create a PlayerAnalysis object with filepath directory
 if __name__ == '__main__':
     main = Main()
-    main.runLinearRegression()
+    main.on_select()
     
 
 
