0f867ec44199b9f3a3a31f0d19b23e1822abd828
kleibo <kleibo1999@gmail.com>
2023-01-22 22:01:00 -0500

    Finalized kmeanscluster Class using plotly

--- KmeansClustering.py
+++ KmeansClustering.py
@@ -2,14 +2,13 @@ import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
-import os
+import plotly.express as px
 from PlayerAnalysis import PlayerAnalysis
 
 class PlayerKmeansClustering(PlayerAnalysis):
     def __init__(self, playerDataFile):
         super().__init__(playerDataFile)
-        self.file_directory = os.path.dirname(playerDataFile)
-        self.model = None
+
 
     def clusterPlayers(self):
         # Create a new DataFrame with player's total theo and total visits
@@ -22,10 +21,32 @@ class PlayerKmeansClustering(PlayerAnalysis):
         # Run Kmeans with 4 clusters
         kmeans = KMeans(n_clusters=4)
         kmeans.fit(playerSummary[['TotlTheo', 'TotlVisit']])
-        playerSummary['Cluster'] = kmeans.labels_
+        playerSummary['Cluster'] = kmeans.predict(playerSummary[['TotlTheo', 'TotlVisit']])
 
 
         return playerSummary
 
-analysis = PlayerKmeansClustering("C:/Users/kleib/Code/kleibo/Projects/Casino-Player-Analysis/Test_Data_10_Accts.csv")
-clustered_players = analysis.clusterPlayers()
+    def plotCluster(self):
+        # Create the scatterplot
+        clusteredPlayers = self.clusterPlayers()
+
+        plt.scatter(clusteredPlayers['TotlVisit'], clusteredPlayers['TotlTheo'], c=clusteredPlayers['Cluster'])
+
+        # Add labels for the x and y axis
+        plt.xlabel('Total Visit')
+        plt.ylabel('Total Theo')
+
+        # Add a title for the plot
+        plt.title('Player Kmeans Clustering')
+
+        # Show plot
+        plt.show()
+
+    def plotClusterspx(self):
+        clusteredPlayers = self.clusterPlayers()
+        fig = px.scatter(clusteredPlayers, x='TotlVisit', y='TotlTheo', color='Cluster', hover_name="PlayerId")
+        fig.show()
+
+analysis = PlayerKmeansClustering("C:/Users/kleib/Code/kleibo/Projects/Casino Data/Test_Data_UnClean - Shortened.csv")
+# analysis.plotCluster()
+analysis.plotClusterspx()
